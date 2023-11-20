e6ece0d1108156f8b2194231c78555a529a3a38c
Rajat Kumar Sahoo <sahoo21rajat@gmail.com>
2023-11-20 16:26:32 +0530

    nav-bar

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const app=express();
+const port=8080;
+const mongoose = require('mongoose');
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate=require("ejs-mate");
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);   //boilerpalte use
+app.use(express.static(path.join(__dirname,"/public")))   //static files use (Ex- style.css)
+
+app.get("/",(req,res)=>{
+    res.send("Root page");
+});
+
+
+//index route
+app.get("/home",async (req,res)=>{
+    res.render("listings/index.ejs");
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server is listening to port ${port}`);
+});
